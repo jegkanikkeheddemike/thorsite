ca7d8e19c3fdd729e5001412274ea130598744a8
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2022-07-03 21:18:09 +0200

    git reset --hard

--- servercontainer/src/main.rs
+++ servercontainer/src/main.rs
@@ -14,6 +14,20 @@ fn spawn() -> Child {
 }
 
 fn pull() {
+    let output = Command::new("git").args(["reset", "--hard"]).output().unwrap();
+
+    println!(
+        "Git reset:\n{}",
+        String::from_utf8(output.stdout).unwrap()
+    );
+
+    let output = Command::new("git").args(["pull"]).output().unwrap();
+
+    println!(
+        "Pulled from github:\n{}",
+        String::from_utf8(output.stdout).unwrap()
+    );
+
     let output = Command::new("git").args(["pull"]).output().unwrap();
 
     println!(
