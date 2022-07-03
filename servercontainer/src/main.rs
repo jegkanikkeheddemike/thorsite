30b7575f9f5b1c3ba9a886d227372d27a71dcf06
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2022-07-03 19:40:00 +0200

    servercontainer included now :)

--- servercontainer/src/main.rs
+++ servercontainer/src/main.rs
@@ -18,6 +18,14 @@ async fn webhook_listen(_: String) -> impl Responder {
     let mut lock = PROCESS.lock().unwrap();
     let _ = lock.as_mut().unwrap().kill();
 
+    //pull
+    let output = Command::new("git")
+        .args(["pull", "https://github.com/jegkanikkeheddemike/thorsite"])
+        .output()
+        .unwrap();
+
+    println!("Pulled from github:\n{}", String::from_utf8(output.stdout).unwrap());
+
     *lock = Some(spawn());
 
     ""
@@ -25,7 +33,6 @@ async fn webhook_listen(_: String) -> impl Responder {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-
     *PROCESS.lock().unwrap() = Some(spawn());
 
     HttpServer::new(|| App::new().service(webhook_listen))
