3cb1d5a6046ad328622ae6b441ae7ddde9e56210
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2022-07-03 21:18:50 +0200

    removed dups

--- servercontainer/src/main.rs
+++ servercontainer/src/main.rs
@@ -27,13 +27,6 @@ fn pull() {
         "Pulled from github:\n{}",
         String::from_utf8(output.stdout).unwrap()
     );
-
-    let output = Command::new("git").args(["pull"]).output().unwrap();
-
-    println!(
-        "Pulled from github:\n{}",
-        String::from_utf8(output.stdout).unwrap()
-    );
 }
 
 #[post("/")]
