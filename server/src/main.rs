f4a3a5701bcd2f0c8345af3c2cc3ea17fddd212b
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2022-07-03 20:22:36 +0200

    static serve

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,31 @@
-use std::path::PathBuf;
+use std::process::Command;
 
-use actix_files::NamedFile;
-use actix_web::{get, App, HttpRequest, HttpServer};
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
+use actix_web_static_files::ResourceFiles;
 
-#[get("/")]
-async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
-    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
-    Ok(NamedFile::open(path)?)
+#[get("/specs")]
+async fn specs(_: HttpRequest) -> impl Responder {
+    let stdout = Command::new("neofetch").output().unwrap().stdout;
+
+    let specs = String::from_utf8_lossy(&stdout).to_string();
+
+    specs
 }
+
+include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("0.0.0.0", 3002))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let generated = generate();
+
+        let mut app = App::new();
+        app = app.service(specs);
+        app = app.service(ResourceFiles::new("/", generated));
+
+        app
+    })
+    .bind(("0.0.0.0", 3002))?
+    .run()
+    .await
 }
