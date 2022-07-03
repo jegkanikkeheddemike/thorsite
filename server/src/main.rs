fc616aed4aab1599fcdc9365f8d487d0a3b0fc80
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2022-07-03 19:50:26 +0200

    static server test

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use actix_files::NamedFile;
+use actix_web::{get, App, HttpRequest, HttpServer};
+
+#[get("/")]
+async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(NamedFile::open(path)?)
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", 3002))?
+        .run()
+        .await
 }
