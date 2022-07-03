6a81c00ca6f278bd3030ef885b7d5c4bb60be4c6
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2022-07-03 20:48:46 +0200

    site stuff

--- thorsite/src/App.tsx
+++ thorsite/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div class="App">
+      <h1>Haha stuff!</h1>
       <header class="App-header">
         <img src={logo} class="App-logo" alt="logo" />
         <p>
